ccf03d38b216deb9934321c897002ca329ac2721
c0b20191 <c0b2019122@edu.teu.ac.jp>
2024-07-25 23:19:49 +0900

    fix

--- AddObservations.cs
+++ AddObservations.cs
@@ -65,6 +65,9 @@ public class AddObservations
         observations.Add(carAgent.speed);
         observations.Add(carAgent.torque);
 
+        if (i == 1 && tag == "car" && Mathf.Abs(carAgent.transform.localPosition.x - otherAgentPosition.x) <= 1.0f)
+        {
+        }
         return observations;
     }
 
--- SlipStream.cs
+++ SlipStream.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class SlipStream
 {
-    
+    private CarAgent carAgent;
+    private RewardCalculation rewardCalculation;
+    public SlipStream(CarAgent carAgent)
+    {
+        this.carAgent = carAgent;
+        this.rewardCalculation = carAgent.rewardCalculation;
+    }
+
+    public void judgeSlipStream(float distance)
+    {
+        
+    }
 }
